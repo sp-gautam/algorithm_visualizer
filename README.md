32d7c4761c4d18c9440ac6da19cb491ab2189af6
Sangh Priya Gautam <84147854+sp-gautam@users.noreply.github.com>
2023-08-22 16:31:37 +0530

    v1

---
+++ README.md
@@ -0,0 +1,37 @@
+## Visualizer
+
+
+This project is developed with React to help people to view algorithmic processes intuitively
+
+## Algorithms Included
+
+- Pathfinding Category
+  - BFS
+  - Dijkstra
+  - A* Search
+  - DFS
+- Sorting Category
+  - Bubble Sort
+  - Insertion Sort
+  - Selection Sort
+  - Merge Sort
+  - Quick Sort
+- AI Category
+  - Simple Perceptron
+  - Approximate Q Learning
+  - Minimax(Alpha–beta pruning)
+
+## How To Use
+
+
+
+
+
+## Future Expectations
+- Implement and complete descriptions & tutorials of different algorithms
+- Customizable learning parameters and model architectures
+- Allowing submissions of codes/models
+- User rankings by their models' performances
+- ...
+
+#### ENJOY THE BEAUTY OF ALGORITHMS!
---
+++ build/precache-manifest.c5f50fc2fa51be3e69de48942252bbd7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ae893aea69b847674d496ce9b672edd",
+    "url": "/Visualizer/index.html"
+  },
+  {
+    "revision": "6ca90465a6796824ab16",
+    "url": "/Visualizer/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "99af5c09d8e239f62db0",
+    "url": "/Visualizer/static/css/main.2ba2c0d6.chunk.css"
+  },
+  {
+    "revision": "6ca90465a6796824ab16",
+    "url": "/Visualizer/static/js/2.04de8505.chunk.js"
+  },
+  {
+    "revision": "172d1c11aa8c10eb2d2d15cd46561efa",
+    "url": "/Visualizer/static/js/2.04de8505.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99af5c09d8e239f62db0",
+    "url": "/Visualizer/static/js/main.5494be56.chunk.js"
+  },
+  {
+    "revision": "bd31e64563914a5d32a4",
+    "url": "/Visualizer/static/js/runtime-main.77790505.js"
+  },
+  {
+    "revision": "710eee821670656c54405649e4e1173d",
+    "url": "/Visualizer/static/media/f.710eee82.svg"
+  },
+  {
+    "revision": "710eee821670656c54405649e4e1173d",
+    "url": "/Visualizer/static/media/finish.710eee82.svg"
+  },
+  {
+    "revision": "ed48baac1772ec46f03b98c3a2953b96",
+    "url": "/Visualizer/static/media/s.ed48baac.svg"
+  },
+  {
+    "revision": "ed48baac1772ec46f03b98c3a2953b96",
+    "url": "/Visualizer/static/media/start.ed48baac.svg"
+  }
+]);
